967c3c985d5ebc733164001ee120ffb6aebd2236
Arman Jindal <arman.jindal@columbia.edu>
2021-01-09 13:13:13 +0530

    Update name extraction code, custom action server build GHA

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,38 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, List, Any, Dict
 
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
-# This is a simple example for a custom action which utters "Hello World!"
+def extractName(slot_value, tracker):
+    """ Take in the slot value extracted using slot mapping 'from_text' 
+    extracts name, and if cannot find returns None"""
+    name_entity = next(tracker.get_latest_entity_values("name"), None)
+    PERSON_entity = next(tracker.get_latest_entity_values("PERSON"), None)
+    resp_words = slot_value.split()
+    print(f"THIS RAN {slot_value}, {name_entity}, {PERSON_entity}, {resp_words}")
+    if name_entity:
+        return name_entity.capitalize()    
+    if PERSON_entity:
+        return PERSON_entity.capitalize()
+    if len(resp_words) == 1:
+        return resp_words[0].capitalize()
+    return None
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ValidateFirstTimeForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_first_form"
+
+    def validate_userName(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        name = extractName(slot_value, tracker)
+        if name:
+            return {"userName":name}
+        else:
+            dispatcher.utter_message(text="Sorry I missed that. Can you _just_ your first name for me?")
+            return {"userName": None}
